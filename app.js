e7b9181caf78922a1500296a2f0954548d12255a
IRodT16 <rodeghier.ian@gmail.com>
2022-10-07 12:33:30 -0400

    Functionality

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ const connectDB = require('./db/connect');
 require('dotenv').config();
 
 // middleware
-
+app.use(express.static('./public'));
 app.use(express.json());
 
 // routes
-app.get('/hello', (req, res) => {
-  res.send('Task Manager App');
-});
+// app.get('/', (req, res) => {
+//   res.send('You fucking rock!');
+// });
+// app.get('/hello', (req, res) => {
+//   res.send('Task Manager App');
+// });
 
 app.use('/api/v1/tasks', tasks);
 
